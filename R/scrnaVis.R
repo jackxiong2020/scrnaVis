fb7656d6e1f36d77fad234b8cae87ad51cfb6678
Xiong Yangjie <xiongyangjie2020@sinh.ac.cn>
2023-03-25 22:16:47 +0800

    up to date

--- R/scrnaVis.R
+++ R/scrnaVis.R
@@ -3,7 +3,7 @@
 #' @param object a seurat object which has run seurat pipeline
 #' @param markers  a gene list of cell-specific expression
 #' @import irGSEA gginnards msigdbr egg ggsci ggplot2 ComplexHeatmap dplyr Seurat
-#' @import ggstatsplot shinydashboard shiny base
+#' @import ggstatsplot shinydashboard shiny
 #' @return shiny
 #' @export
 #' @examples
